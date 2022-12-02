d08c75d548a7734e3a92af30476338e2cd266d2f
Daryl_Moh <Daryl.Moh@hotmail.com>
2022-12-02 16:19:40 +0800

    Day 02 - Workshop first commit

---
+++ src/prog/Main.java
@@ -0,0 +1,44 @@
+package prog;
+
+import cart.Item;
+import cart.Apple;
+import cart.Orange;
+
+public class Main{
+
+    public static void main(String[] args) {
+        /*
+        Item apple = new Item("apple", "Apple");
+        Item orange = new Item("orange", "Orange");
+
+        apple.setPrice(.5f);
+        apple.setQuantity(10);
+
+        orange.setPrice(0.7f);
+        orange.setQuantity(15);
+         */
+        Apple apple = new Apple();
+        apple.setType = ("washington");
+        Orange orange = new Orange();
+        
+        System.out.printf("apple code: %s\n", apple.getCode());
+        System.out.printf("orange code: %s\n", orange.getCode());
+
+        Item item = apple;
+        System.out.printf("item code: %s\n", item.getCode());
+
+        //Casting
+        Apple washington = (Apple)item;
+        System.out.printf("washington code: %s\n", washington.getCode());
+        // orange = (Orange) item but item already contains apple
+        System.out.printf("Is item and orange? %b\n", item instanceof Orange);
+        if (item instanceof Orange)
+            orange = (Orange)item;
+
+    }
+
+    
+    
+
+}
+
